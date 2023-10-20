4b7da76991925251930868d9e6c21b51a229b5a0
olgavrou <olgavrou@gmail.com>
2023-10-20 12:16:26 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="learn_to_pick",
+    version="0.1",
+    install_requires=[
+        'numpy',
+        'pandas',
+        'vowpal-wabbit-next',
+        'sentence-transformers',
+        'torch',
+        'pyskiplist',
+        'parameterfree',
+    ],
+    author="VowpalWabbit",
+    description="",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    url="https://github.com/VowpalWabbit/learn_to_pick",
+    python_requires='>=3.8',
+)
